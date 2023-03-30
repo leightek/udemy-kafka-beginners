25a3e29bfc2efa7f02f9edd3b9819a45cd60a349
Leigh Su <slhcan@yahoo.com>
2023-03-29 23:11:02 -0700

    [KafkaBeginners] Added logging

--- kafka-basics/src/main/java/com/leightek/demos/kafka/ProducerDemo.java
+++ kafka-basics/src/main/java/com/leightek/demos/kafka/ProducerDemo.java
@@ -1,7 +1,20 @@
 package com.leightek.demos.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ProducerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        log.info("Hello world!");
+
+        // create Producer Properties
+        
+        // create the Producer
+
+        // send data
+
+        // flush and close the producer
     }
 }
