4ef70823efc4635f010f70fa7714525e27508073
Leigh Su <slhcan@yahoo.com>
2023-03-31 00:49:23 -0700

    [KafkaBeginners] Add RoundRobinPartitioner class but comment out

--- kafka-basics/src/main/java/com/leightek/demos/kafka/ProducerDemoWithCallback.java
+++ kafka-basics/src/main/java/com/leightek/demos/kafka/ProducerDemoWithCallback.java
@@ -1,9 +1,6 @@
 package com.leightek.demos.kafka;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,6 +30,8 @@ public class ProducerDemoWithCallback {
         properties.setProperty("value.serializer", StringSerializer.class.getName());
 
         properties.setProperty("batch.size", "400");
+
+//        properties.setProperty("partitioner.class", RoundRobinPartitioner.class.getName());
 
         // create the Producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
