92a4d49573760c8e3c67705064f1c213a2f50515
Leigh Su <slhcan@yahoo.com>
2023-03-31 00:18:09 -0700

    [KafkaBeginners] Added producer with callback

--- kafka-basics/src/main/java/com/leightek/demos/kafka/ProducerDemoWithCallback.java
+++ kafka-basics/src/main/java/com/leightek/demos/kafka/ProducerDemoWithCallback.java
@@ -1,15 +1,17 @@
 package com.leightek.demos.kafka;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
-public class ProducerDemo {
-    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+public class ProducerDemoWithCallback {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoWithCallback.class.getSimpleName());
 
     public static void main(String[] args) {
         log.info("I am a kafka Producer!");
@@ -37,7 +39,22 @@ public class ProducerDemo {
         ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world");
 
         // send data
-        producer.send(producerRecord);
+        producer.send(producerRecord, new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata metadata, Exception e) {
+                // executes every time a record successfully sent or exception is thrown
+                if (e == null) {
+                    // the record was successfully sent
+                    log.info("Received new metadata \n" +
+                            "Topic: " + metadata.topic() + "\n" +
+                            "Partition: " + metadata.partition() + "\n" +
+                            "Offset: " + metadata.offset() + "\n" +
+                            "Timestamp: " + metadata.timestamp());
+                } else {
+                    log.error("Error while producing", e);
+                }
+            }
+        });
 
         // tell the producer to send all data and block util done ... synchronous
         producer.flush();
