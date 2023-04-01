e0f5b9ee40ff219a7742882b1dc8edb6504c74eb
Leigh Su <slhcan@yahoo.com>
2023-04-01 00:23:46 -0700

    [KafkaBeginners] Added producer using key/value

---
+++ kafka-basics/src/main/java/com/leightek/demos/kafka/ProducerDemoKeys.java
@@ -0,0 +1,76 @@
+package com.leightek.demos.kafka;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoKeys {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoKeys.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am a kafka Producer!");
+
+        // create Producer Properties
+        Properties properties = new Properties();
+
+        // connect to local host
+//        properties.setProperty("bootstrap.servers", "SASL_SSL");
+
+        // connect to Conductor Playground
+        properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
+        properties.setProperty("security.protocol", "SASL_SSL");
+        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"1AbCcIoadCyQ27rFRgZaaH\" password=\"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2F1dGguY29uZHVrdG9yLmlvIiwic291cmNlQXBwbGljYXRpb24iOiJhZG1pbiIsInVzZXJNYWlsIjpudWxsLCJwYXlsb2FkIjp7InZhbGlkRm9yVXNlcm5hbWUiOiIxQWJDY0lvYWRDeVEyN3JGUmdaYWFIIiwib3JnYW5pemF0aW9uSWQiOjcxMTEzLCJ1c2VySWQiOjgyMzgwLCJmb3JFeHBpcmF0aW9uQ2hlY2siOiJjMTk3YzZkZC1kN2MyLTRhNmYtOWI2OS00NDEyMmFhZWQ2Y2EifX0.kCkvvIQYZ498fcnmFVedz340FRaMunJPy9C8dyJLm8k\";");
+        properties.setProperty("sasl.mechanism", "PLAIN");
+
+        // set producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        for (int j = 0; j < 2; j++) {
+            for (int i = 0; i < 10; i++) {
+
+                String topic = "demo_java";
+                String key = "id_" + i;
+                String value = "hello world " + i;
+
+                // create a Producer Record
+                ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topic, key, value);
+
+                // send data
+                producer.send(producerRecord, new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata metadata, Exception e) {
+                        // executes every time a record successfully sent or exception is thrown
+                        if (e == null) {
+                            // the record was successfully sent
+                            log.info("Key: " + key + " Partition: " + metadata.partition());
+                        } else {
+                            log.error("Error while producing", e);
+                        }
+                    }
+                });
+            }
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // tell the producer to send all data and block util done ... synchronous
+        producer.flush();
+
+        // flush and close the producer
+        producer.close();
+    }
+}
