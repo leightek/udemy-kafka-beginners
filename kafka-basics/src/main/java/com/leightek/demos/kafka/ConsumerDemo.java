7caf74897661c46e8c783e034c5d77f87bf33345
Leigh Su <slhcan@yahoo.com>
2023-04-01 00:59:26 -0700

    [KafkaBeginners] Added consumer

---
+++ kafka-basics/src/main/java/com/leightek/demos/kafka/ConsumerDemo.java
@@ -0,0 +1,61 @@
+package com.leightek.demos.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ConsumerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am a kafka Consumer!");
+
+        String groupId = "my-java-application";
+        String topic = "demo_java";
+
+        // create Producer Properties
+        Properties properties = new Properties();
+
+        // connect to local host
+//        properties.setProperty("bootstrap.servers", "SASL_SSL");
+
+        // connect to Conductor Playground
+        properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
+        properties.setProperty("security.protocol", "SASL_SSL");
+        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"1AbCcIoadCyQ27rFRgZaaH\" password=\"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2F1dGguY29uZHVrdG9yLmlvIiwic291cmNlQXBwbGljYXRpb24iOiJhZG1pbiIsInVzZXJNYWlsIjpudWxsLCJwYXlsb2FkIjp7InZhbGlkRm9yVXNlcm5hbWUiOiIxQWJDY0lvYWRDeVEyN3JGUmdaYWFIIiwib3JnYW5pemF0aW9uSWQiOjcxMTEzLCJ1c2VySWQiOjgyMzgwLCJmb3JFeHBpcmF0aW9uQ2hlY2siOiJjMTk3YzZkZC1kN2MyLTRhNmYtOWI2OS00NDEyMmFhZWQ2Y2EifX0.kCkvvIQYZ498fcnmFVedz340FRaMunJPy9C8dyJLm8k\";");
+        properties.setProperty("sasl.mechanism", "PLAIN");
+
+        // create consumer configs
+        properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("value.deserializer", StringDeserializer.class.getName());
+
+        properties.setProperty("group.id", groupId);
+
+        properties.setProperty("auto.offset.reset", "earliest");   // none/earliest/latest
+
+        // create a consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // subscribe to a topic
+        consumer.subscribe(Arrays.asList(topic));
+
+        // poll for data
+        while (true) {
+            log.info("Polling");
+
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+
+            for (ConsumerRecord<String, String> record : records) {
+                log.info("Key: " + record.key() + ", Value: " + record.value());
+                log.info("Partition}: " + record.partition() + ", Offset: " + record.offset());
+            }
+        }
+    }
+}
